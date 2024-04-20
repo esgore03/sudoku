210ea17fd7ff2c62f168875e2047696154060a4a
esgore03 <143649079+esgore03@users.noreply.github.com>
2024-04-20 16:00:42 -0500

    a

--- src/main/java/org/example/sudoku/controller/GameController.java
+++ src/main/java/org/example/sudoku/controller/GameController.java
@@ -45,57 +45,10 @@ public class GameController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        createSudokuBoard();
-        addBordersToBoxes();
+
     }
 
-    private void addBordersToBoxes() {
-        for (int i = 0; i < SIZE; i += BOX_SIZE) {
-            for (int j = 0; j < SIZE; j += BOX_SIZE) {
-                addBorderToBox(i, j);
-            }
-        }
-    }
 
-    private void addBorderToBox(int startRow, int startCol) {
-        BorderPane borderPane = new BorderPane();
-        borderPane.setStyle("-fx-border-color: black; -fx-border-width: 2px;");
-
-        for (int row = startRow; row < startRow + BOX_SIZE; row++) {
-            for (int col = startCol; col < startCol + BOX_SIZE; col++) {
-                Rectangle cell = new Rectangle(50, 50);
-                cell.setFill(Color.WHITE);
-                cell.setStroke(Color.BLACK);
-                Pane pane = new Pane(cell);
-                borderPane.setCenter(pane);
-            }
-        }
-
-        gridPane.add(borderPane, startCol, startRow);
-    }
-
-    private void createSudokuBoard() {
-        double cellSize = 50;
-        double boxSize = cellSize * BOX_SIZE; // Tamaño del grupo 3x3
-
-        for (int row = 0; row < SIZE; row += BOX_SIZE) {
-            for (int col = 0; col < SIZE; col += BOX_SIZE) {
-                GridPane boxPane = new GridPane();
-                boxPane.setPadding(new Insets(2)); // Espaciado entre los rectángulos en el grupo
-
-                for (int i = 0; i < BOX_SIZE; i++) {
-                    for (int j = 0; j < BOX_SIZE; j++) {
-                        Rectangle cell = new Rectangle(cellSize, cellSize);
-                        cell.setFill(Color.WHITE);
-                        cell.setStroke(Color.BLACK);
-                        boxPane.add(cell, j, i);
-                    }
-                }
-
-                sudokuGridPane.add(boxPane, col, row);
-            }
-        }
-    }
 
     public void setPlayer(Player player) {
         this.player = player;
