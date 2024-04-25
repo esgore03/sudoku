796ed38488423abcb467463de1b41977a58b4e5b
es-sai-fi <jhormangomez2005@outlook.com>
2024-04-24 19:02:33 -0500

    GameStage

--- src/main/java/org/example/sudoku/controller/GameController.java
+++ src/main/java/org/example/sudoku/controller/GameController.java
@@ -1,13 +1,24 @@
 package org.example.sudoku.controller;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import org.example.sudoku.model.BoardNum;
 import org.example.sudoku.model.SudokuGame;
 
+import java.io.IOException;
+
 public class GameController{
     @FXML
-    private GridPane gridPane;
+    private Button verifyButton;
+    @FXML
+    private Button resolveButton;
+    @FXML
+    private GridPane board;
     private SudokuGame sudokuGame = new SudokuGame();
     BoardNum[][] sudokuBoard = new BoardNum[9][9];
     public void initialize() {
@@ -19,14 +30,30 @@ public class GameController{
         for (int row = 0; row < 9; row++) {
             for (int column = 0; column < 9; column++) {
                 TextField textField = new TextField();
+                textField.setMinWidth(25);
+                textField.setMinHeight(25);
+                Rectangle rectangle = new Rectangle();
+                rectangle.setFill(Color.WHITE);
+                rectangle.setStroke(Color.BLACK);
+                rectangle.setWidth(50);
+                rectangle.setHeight(50);
+                StackPane stackPane = new StackPane();
+                stackPane.getChildren().addAll(rectangle, textField);
                 textField.setText("");
-                gridPane.add(textField, column, row);
+                board.add(stackPane, column, row);
                 BoardNum boardNum = new BoardNum(textField, row, column);
                 sudokuBoard[row][column] = boardNum;
             }
         }
     }
 
+    @FXML
+    void onVerifyButtonClick(ActionEvent actionEvent) throws IOException{
+    }
+
+    @FXML
+    void onResolveButtonClick(ActionEvent actionEvent) throws IOException{
+    }
 }
 
 
